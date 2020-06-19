81186f56117a0ebcb8ab05e68b87547156a63be4
rregalado17 <rregalado17@gmail.com>
2020-06-19 02:01:29 -0400

    skeletal backend

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,14 @@
 class Player < ApplicationRecord
   belongs_to :team
+  
+  validate do 
+    player_count_valid?
+  end
+
+  private 
+  def player_count_valid?
+    if self.team.players.count >= 5
+      self.errors.add(:team_max, "You can only have 5 players on your team")
+    end
+  end
 end
