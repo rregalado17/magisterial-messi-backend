888ff8de086dc264fcb70a3616cdaf17350dc611
rregalado17 <rregalado17@gmail.com>
2020-06-20 19:16:57 -0400

    update controllers with destroy, create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class PlayersController < ApplicationController
     def index
         players = Player.all 
@@ -7,5 +9,18 @@ class PlayersController < ApplicationController
     def show
         player = Player.find(params[:id])
         render json: player
+    end
+
+    def create 
+        team = Team.find(params[:team_id])
+        player = team.players.build({
+            name: Faker::Name.first_name
+        })
+        render json: player.save ?  player : {message: player.errors.messages[:team_max][0]}
+    end
+
+    def destroy 
+        player = Player.find(params[:id])
+        player.destroy
     end
 end
